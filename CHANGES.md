a5160423701d6c2b2e81fa5c1be5973273b8a3af
UH <554c46@gmail.com>
2019-11-29 11:46:37 +0100

    line profiler removed and profile nbs merged

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,8 @@
+# 0.1.3 / 2019-11-29
+
+  * Line Profiler removed from deps
+  * Profile notebooks merged
+
 # 0.1.2 / 2019-02-17
 
   * allow to set nacode manually
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='grouplabelencode',
-      version='0.1.2',
+      version='0.1.3',
       description='Encode grouped labels',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
